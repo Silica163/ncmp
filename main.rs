4bf59f29ab04d76faa21a953298e7bcbcc8539cf
silica163 <pattarakon_m@hotmail.com>
2025-06-29 17:05:21 +0700

    Suffle playlist and track played song.

--- main.rs
+++ main.rs
@@ -20,15 +20,45 @@ macro_rules! c {
 }
 
 macro_rules! sleep {
-    ($ms:expr) =>{
+    ($ms:expr) => {
         thread::sleep(time::Duration::from_millis($ms));
     };
+}
+
+macro_rules! time_rand {
+    ($max:expr) => {
+        (time::SystemTime::now().duration_since(time::SystemTime::UNIX_EPOCH).unwrap().as_micros() as usize) % $max
+    }
 }
 
 fn try_exit(){
     ma_wrapper::uninit();
     println!();
     process::exit(1);
+}
+
+#[derive(Debug, Clone)]
+struct PlaylistItem {
+    file: String,
+    file_idx: usize,
+    played: bool,
+}
+
+impl PlaylistItem {
+    pub fn new_empty() -> Self {
+        Self {
+            file: "".to_string(),
+            file_idx: 0,
+            played: false,
+        }
+    }
+    pub fn new(idx: usize, file: String) -> Self {
+        Self {
+            file: file,
+            file_idx: idx,
+            played: false,
+        }
+    }
 }
 
 fn main() {
@@ -41,21 +71,49 @@ fn main() {
     }
 
     let mut audio_files: Vec<String> = vec![];
+    let mut playlist: Vec<PlaylistItem> = vec![];
     for i in 1..args.len() {
         audio_files.push(args[i].clone());
+        playlist.push(PlaylistItem::new_empty());
     }
     println!("{audio_files:?}");
+
+    for i in 0..audio_files.len() {
+        let idx = {
+            let mut idx = time_rand!(audio_files.len());
+            while playlist[idx].file != "" {
+                idx = time_rand!(audio_files.len());
+            }
+            idx
+        };
+        playlist[idx] = PlaylistItem::new(i, audio_files[i].clone());
+    }
 
     let mut player_status = ma_wrapper::PlayerStatus { playing: 0, ended: 0, pause: 0, };
     ma_wrapper::init(&player_status);
     thread::spawn( move || {
-        for i in 0..audio_files.len() {
-            let file = audio_files[i].clone();
-            println!("Playing: {}", file.rsplitn(2,"/").collect::<Vec<&str>>()[0]);
-            ma_wrapper::play(file);
-            while !ma_wrapper::is_ended() {
-                sleep!(100);
+        let mut playlist_ended = false;
+        let mut play_all = true;
+        let mut song_idx = 0;
+        while !playlist_ended {
+            let song = playlist[song_idx].clone();
+            if !song.played {
+                let file = song.file.clone();
+                println!("Playing: {}", file.rsplitn(2,"/").collect::<Vec<&str>>()[0]);
+                ma_wrapper::play(file);
+                while !ma_wrapper::is_ended() {
+                    sleep!(100);
+                }
+                playlist[song_idx].played = true;
             }
+
+            song_idx += 1;
+            if song_idx >= playlist.len() {
+                song_idx = 0;
+                play_all = true;
+            }
+            play_all &= playlist[song_idx].played;
+            playlist_ended = play_all;
         }
         try_exit();
     });
